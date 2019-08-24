58ae814096b014c4efb9f67b743b57d78f4af39b
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-24 19:18:54 +0900

    rename directory

--- source/CameraSystem.py
+++ source/CameraSystem.py
@@ -1,9 +1,9 @@
-from numberDetection import DetectionNumber as dn
+from DetectionNumber import DetectionNumber
 
 
 class CameraSystem:
     def __init__(self):
-        self.detection_number = dn.DetectionNumber()
+        self.detection_number = DetectionNumber()
 
     def main(self):
         pass
