0031ef9151b27b7f1c7983369429b4ad4569285a
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-24 16:08:19 +0900

    class化

--- source/numberDetection/detection_number.py
+++ source/numberDetection/detection_number.py
@@ -18,47 +18,51 @@ import glob
 import os
 from clipping_number import clipNumber, captureImage
 
-data_directory = "data"
-imgs_directory = "imgs"  # 画像を保管するディレクトリ
 
+class DetectionNumber:
+    def __init__(self, img=None):
+        self.origin_img = img
+        self.preprocess_img = None
+        self.detected_number = None
+        self.number_model_path = 'my_model.npz'
+        self.net = L.Classifier(MLP(1000, 10))
+        self.data_directory = "data"
+        self.img_directory = "imgs"  # 画像を保管するディレクトリ
+        self.setup()
 
-def preprocessing(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-    # 画像を28x28に縮小
-    img = cv2.resize(img, (28, 28))
-    cv2.imwrite("preprocess.jpg", img)
-    img = img.astype(np.float32)
-    img = np.array(img).reshape(1, 784)
-    return img
+    def setup(self):
+        # 学習済みモデルの読み込み
+        serializers.load_npz(self.number_model_path, self.net)
+        optimizer = chainer.optimizers.Adam()
+        optimizer.setup(self.net)
 
+    def _preprocessing(self, is_save=False):
+        if self.origin_img is None:
+            raise FileNotFoundError('数字画像が指定されていません')
+        img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+        # 画像を28x28に縮小
+        img = cv2.resize(img, (28, 28))
+        if is_save:
+            cv2.imwrite("./imgs/preprocess.jpg", img)
+        img = img.astype(np.float32)
+        img = np.array(img).reshape(1, 784)
+        self.preprocess_img = img
 
-def get_image():
-    # ラズパイから映像を受信し、保存する
-    src_name = captureImage(target_dir=imgs_directory)
-    target_name = "_result_" + src_name  # 切り取った画像の出力ファイル名
+    def set_img(self, img):
+        self.origin_img = img
 
-    # 画像を切り取り、保存する
-    clipNumber(src_path=os.path.join(imgs_directory, src_name), target_name=target_name, target_dir=imgs_directory)
-    img = cv2.imread(imgs_directory + target_name)
-    return img
-
-
-def get_detect_number(img):
-    # 学習済みモデルの読み込み
-    net = L.Classifier(MLP(1000, 10))
-    serializers.load_npz('my_model.npz', net)
-    optimizer = chainer.optimizers.Adam()
-    optimizer.setup(net)
-    num = net.predictor(img)
-    return np.argmax(num.data) + 1
+    def get_detect_number(self, is_save=False):
+        self._preprocessing(is_save)
+        num = self.net.predictor(self.preprocess_img)
+        return np.argmax(num.data) + 1
 
 
 def main():
     # Webカメラの映像とりこみ
-    img = get_image()
-    img = preprocessing(img)
-    number = get_detect_number(img)
+    img = cv2.imread("./imgs/sample_number.jpg")
+    detection_number = DetectionNumber(img)
+    number = detection_number.get_detect_number(is_save=True)
     print(number)
 
 
