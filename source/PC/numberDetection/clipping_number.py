0df7ab53442ff1d9b3871f575b395ff0edecd31d
Takahiro <tkhr.corei7@gmail.com>
2019-08-24 20:33:20 +0900

    Update: 範囲選択できるようになった

--- source/PC/numberDetection/clipping_number.py
+++ source/PC/numberDetection/clipping_number.py
@@ -12,18 +12,12 @@ import numpy as np
 
 from get_point import get_point, PointList
 
-def captureImage(target_name=None, target_dir="./", url="http://192.168.11.25/?action=stream", padding=0):
+def captureImage(url="http://raspberrypi.local/?action=stream", padding=0):
     """
     URLから流れてくる映像をキャプチャし、静止画として保存する
 
     Parameters
     ----------
-    target_name: str
-        キャプチャした静止画のファイル名。デフォルトでは時刻によってファイル名が決まる。
-
-    target_dir: str
-        キャプチャした静止画を保存するためのディレクトリ。デフォルトでは実行時のカレントディレクトリ。
-
     url: str
         映像配信URL
     
@@ -32,13 +26,14 @@ def captureImage(target_name=None, target_dir="./", url="http://192.168.11.25/?a
 
     Returns
     -------
-    target_name: str
-        キャプチャした静止画のファイル名
+    target_name: numpy.ndarray
+        キャプチャした静止画
     """
     # VideoCaptureのインスタンスを作成する。
     cap = cv2.VideoCapture(url) # カメラシステムを使う場合
     #cap = cv2.VideoCapture(DEVICE_ID) # このpythonスクリプトを実行するPCのカメラを使う場合コメントアウトして下さい
     if not cap.isOpened():
+        # HACK: エラーを返した方がいいかも
         print("On file {}".format(__file__))
         print("画像のキャプチャに失敗しました")
         sys.exit()
@@ -57,10 +52,6 @@ def captureImage(target_name=None, target_dir="./", url="http://192.168.11.25/?a
 
     # 画像をキャプチャ
     ret, img = cap.read()
-    
-    # 出力ファイル名を設定
-    if target_name == None:
-        target_name = 'snapshot_{0:%Y%m%d_%H%M%S}.jpg'.format(datetime.datetime.now())
 
     # 余白を設定する
     tmp = img[:, :]
@@ -68,16 +59,13 @@ def captureImage(target_name=None, target_dir="./", url="http://192.168.11.25/?a
     new_img = cv2.resize(np.full((1, 1, 3), fill_value=255, dtype=np.uint8), dsize=(width+padding*2, height+padding*2))
     new_img[padding:height+padding, padding:width+padding] = tmp
 
-    # 画像をJPEGファイルへ保存
-    cv2.imwrite(target_dir + target_name, new_img)
-
     # キャプチャ終了
     cap.release()
 
     # ファイル名を返す
-    return target_name
+    return new_img
 
-def clipNumber(src_path, target_name, target_dir="./", output_size=[420, 297],
+def clipNumber(src_img, output_size=[420, 297],
                l_top=[-30, 460], l_btm=[190, 620],
                r_top=[400, 450], r_btm=[200, 360]):
     """
@@ -85,14 +73,8 @@ def clipNumber(src_path, target_name, target_dir="./", output_size=[420, 297],
 
     Parameters
     ----------
-    src_path: str
-        対象のファイル名
-    
-    target_name: str
-        出力するファイル名
-
-    target_dir: str
-        出力先のディレクトリ名
+    src_img: numpy.ndarray
+        入力画像
 
     output_size: list
         出力画像のサイズ。[高さ、横幅]の順
@@ -111,40 +93,30 @@ def clipNumber(src_path, target_name, target_dir="./", output_size=[420, 297],
     
     Returns
     -------
-        None
+    clipped_img: numpy.ndarray
+        切り取った結果の画像
 
     Referrenced
     -----------
      https\://note.nkmk.me/python-opencv-warp-affine-perspective/
     """
-    # ファイル読込み
-    img = cv2.imread(src_path)
-    
     # 出力サイズを引数から取得
     h, w = output_size
+    # [左上の座標],[右上の座標],[左下の座標],[右下の座標]
+    src_pts = np.array([l_top, r_top, l_btm, r_btm], dtype=np.float32)
+    dst_pts = np.array([[0, 0], [w, 0], [0, h], [w, h]], dtype=np.float32)
+    mat = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
-    src_pts = np.array([[0, 0], [0, h], [w, h], [w, 0]], dtype=np.float32)
-    # 座標指定の順序[[左上], [左下], [右上], [右下]]
-    dst_pts = np.array([l_top, l_btm, r_top, r_btm], dtype=np.float32)
-    mat = cv2.getPerspectiveTransform(dst_pts, src_pts)
-    
-    # borderValueで色(R, G, B)256段階を指定すると、画像でない部分を指定した色で埋めてくれる
-    # ２値化することを考慮し、画像でない部分は白色に設定
-    perspective_img = cv2.warpPerspective(img, mat, (w, h), borderValue=(255, 255, 255))
+    clipped_img = cv2.warpPerspective(img, mat, (w, h))
 
-    # 画像の保存
-    cv2.imwrite(target_dir + target_name, perspective_img)
+    return clipped_img
 
 
 if __name__ == '__main__':
-    imgs_dir = "imgs/" # 画像を保管するディレクトリ
-
     # ラズパイから映像を受信し、保存する
     url = "http://raspberrypi.local/?action=stream"
-    src_name = captureImage(target_dir=imgs_dir, url=url, padding=100)
-    target_name = "_result_" + src_name # 切り取った画像の出力ファイル名
-
-    img = cv2.imread(imgs_dir + src_name)
+    img = captureImage(url=url, padding=100)
+    
     wname = "MouseEvent"
     cv2.namedWindow(wname)
     npoints = 4
@@ -156,12 +128,10 @@ if __name__ == '__main__':
     ptlist.trans()
 
     # 画像を切り取り、保存する
-    clipNumber(src_path=imgs_dir + src_name, target_name=target_name, target_dir=imgs_dir, 
-               l_top=ptlist.named_points["l_top"], l_btm=ptlist.named_points["l_btm"], 
-               r_btm=ptlist.named_points["r_top"], r_top=ptlist.named_points["r_btm"])
+    result_img = clipNumber(img, l_top=ptlist.named_points["l_top"], l_btm=ptlist.named_points["l_btm"], 
+               r_btm=ptlist.named_points["r_btm"], r_top=ptlist.named_points["r_top"])
 
     # 台形補正の結果を表示（何かキーを押すと終了）
-    img = cv2.imread(imgs_dir + target_name)
-    cv2.imshow("color", img)
+    cv2.imshow("color", result_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
