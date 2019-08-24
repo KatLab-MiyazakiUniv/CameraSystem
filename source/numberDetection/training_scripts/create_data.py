4e0fd41167081a9f1c526e0f9b23bc4c6b569c60
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-24 15:34:31 +0900

    refactor

--- source/numberDetection/training_scripts/create_data.py
+++ source/numberDetection/training_scripts/create_data.py
@@ -21,21 +21,24 @@ def load_original():
         images.append(cv2.imread(image_address))
     return images
 
+
 def binaryzation(image):
-    #グレースケールへの変換
+    # グレースケールへの変換
     img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, img = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
-    #画像を28x28に縮小
+    _, img = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    # 画像を28x28に縮小
     img = cv2.resize(img, (28, 28))
     return img
 
-def getMeanColor(image):
+
+def get_mean_color(image):
     # RGB平均値を出力
     # flattenで一次元化しmeanで平均を取得 
     b = image.T[0].flatten().mean()
     g = image.T[1].flatten().mean()
     r = image.T[2].flatten().mean()
     return {"r": r, "g": g, "b": b}
+
 
 def change_angle(image):
     # 中心位置取得
@@ -54,12 +57,14 @@ def change_angle(image):
     affine_mat = cv2.getRotationMatrix2D(center, angle, scale)
 
     # 平均色の取得
-    mean =  getMeanColor(image)
+    mean = get_mean_color(image)
     # アフィン変換(回転)
     height, width = image.shape[:2]
-    rotation_image = cv2.warpAffine(image, affine_mat, (width, height), flags=cv2.INTER_CUBIC, borderValue=(mean["b"], mean["g"], mean["r"]))
+    rotation_image = cv2.warpAffine(image, affine_mat, (width, height), flags=cv2.INTER_CUBIC,
+                                    borderValue=(mean["b"], mean["g"], mean["r"]))
 
     return rotation_image
+
 
 def change_brightness(image):
     # 乱数生成
@@ -70,7 +75,7 @@ def change_brightness(image):
     # ルックアップテーブルの生成
     look_up_table = np.zeros((256, 1), dtype=np.uint8)
     for i in range(256):
-        look_up_table[i][0] = 255 * (float(i)/255) ** (1.0 / gamma)
+        look_up_table[i][0] = 255 * (float(i) / 255) ** (1.0 / gamma)
 
     # γ変換後の画像取得
     image_gamma = cv2.LUT(image, look_up_table)
@@ -87,6 +92,7 @@ def print_image(image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 def create_labels(dir_name):
     image_list = glob.glob(dir_name + "*")
 
@@ -95,13 +101,14 @@ def create_labels(dir_name):
         file_name = image[len(dir_name):]
         label = str(int(file_name[:1]) - 1)
         label_list.append(file_name + " " + label)
-    
+
     # ファイルを書き込みモードで開く
     name = dir_name.split("/")[0]
-    text = "\n".join(label_list)             # 改行コードでつなぐ
+    text = "\n".join(label_list)  # 改行コードでつなぐ
     with open(name + "/" + name + '_labels.txt', 'w') as f:
         # 書き込み
         f.write(text)
+
 
 def create_dataset(name, iteration=100):
     images = load_original()
@@ -111,7 +118,7 @@ def create_dataset(name, iteration=100):
     dir_name = name + "/images/"
     shutil.rmtree(dir_name)
     os.mkdir(dir_name)
-    
+
     for original_image in tqdm(images, desc='1st loop'):
         for i in trange(iteration, desc='2nd loop'):
             image = change_brightness(original_image)
@@ -121,6 +128,7 @@ def create_dataset(name, iteration=100):
             save_image(image, name, dir_name)
         num_key += 1
     create_labels(dir_name)
+
 
 if __name__ == "__main__":
     create_dataset("train", 1000)
