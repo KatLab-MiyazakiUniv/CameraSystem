5eb21e63824eb91423fbc5fc2df9fdb9be35833d
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-26 12:58:10 +0900

    change method name

--- source/PointList.py
+++ source/PointList.py
@@ -7,7 +7,7 @@ import cv2
 
 
 class PointList:
-    def __init__(self, npoints):
+    def __init__(self, npoints=4):
         self.npoints = npoints
         self.ptlist = np.empty((npoints, 2), dtype=int)
         self.pos = 0
@@ -30,39 +30,45 @@ class PointList:
         self.named_points["l_btm"] = self.ptlist[np.argmax(y_list)]
         print(self.named_points)
 
+    def add_point(self, img, wname="MouseEvent"):
+        cv2.setMouseCallback(wname, self._add_point, [wname, img, self])
+        cv2.imshow(wname, img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
-def get_point(event, x, y, flag, params):
-    wname, img, ptlist = params
-    if event == cv2.EVENT_MOUSEMOVE:  # マウスが移動したときにx線とy線を更新する
-        img2 = np.copy(img)
-        h, w = img2.shape[0], img2.shape[1]
-        cv2.line(img2, (x, 0), (x, h - 1), (255, 0, 0))
-        cv2.line(img2, (0, y), (w - 1, y), (255, 0, 0))
-        cv2.imshow(wname, img2)
+    @staticmethod
+    def _add_point(event, x, y, flag, params):
+        wname, img, ptlist = params
+        if event == cv2.EVENT_MOUSEMOVE:  # マウスが移動したときにx線とy線を更新する
+            img2 = np.copy(img)
+            h, w = img2.shape[0], img2.shape[1]
+            cv2.line(img2, (x, 0), (x, h - 1), (255, 0, 0))
+            cv2.line(img2, (0, y), (w - 1, y), (255, 0, 0))
+            cv2.imshow(wname, img2)
 
-    if event == cv2.EVENT_LBUTTONDOWN:  # レフトボタンをクリックしたとき、ptlist配列にx,y座標を格納する
-        if ptlist.add(x, y):
-            print('[%d] ( %d, %d )' % (ptlist.pos - 1, x, y))
-            cv2.circle(img, (x, y), 3, (0, 0, 255), 3)
-            cv2.imshow(wname, img)
-        else:
-            print('All points have selected.  Press ESC-key.')
-        if(ptlist.pos == ptlist.npoints):
-            print(ptlist.ptlist)
-            cv2.line(img, (ptlist.ptlist[0][0], ptlist.ptlist[0][1]),
-                     (ptlist.ptlist[1][0], ptlist.ptlist[1][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[1][0], ptlist.ptlist[1][1]),
-                     (ptlist.ptlist[2][0], ptlist.ptlist[2][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[2][0], ptlist.ptlist[2][1]),
-                     (ptlist.ptlist[3][0], ptlist.ptlist[3][1]), (0, 255, 0), 3)
-            cv2.line(img, (ptlist.ptlist[3][0], ptlist.ptlist[3][1]),
-                     (ptlist.ptlist[0][0], ptlist.ptlist[0][1]), (0, 255, 0), 3)
+        if event == cv2.EVENT_LBUTTONDOWN:  # レフトボタンをクリックしたとき、ptlist配列にx,y座標を格納する
+            if ptlist.add(x, y):
+                print('[%d] ( %d, %d )' % (ptlist.pos - 1, x, y))
+                cv2.circle(img, (x, y), 3, (0, 0, 255), 3)
+                cv2.imshow(wname, img)
+            else:
+                print('All points have selected.  Press ESC-key.')
+            if ptlist.pos == ptlist.npoints:
+                print(ptlist.ptlist)
+                cv2.line(img, (ptlist.ptlist[0][0], ptlist.ptlist[0][1]),
+                         (ptlist.ptlist[1][0], ptlist.ptlist[1][1]), (0, 255, 0), 3)
+                cv2.line(img, (ptlist.ptlist[1][0], ptlist.ptlist[1][1]),
+                         (ptlist.ptlist[2][0], ptlist.ptlist[2][1]), (0, 255, 0), 3)
+                cv2.line(img, (ptlist.ptlist[2][0], ptlist.ptlist[2][1]),
+                         (ptlist.ptlist[3][0], ptlist.ptlist[3][1]), (0, 255, 0), 3)
+                cv2.line(img, (ptlist.ptlist[3][0], ptlist.ptlist[3][1]),
+                         (ptlist.ptlist[0][0], ptlist.ptlist[0][1]), (0, 255, 0), 3)
 
 
-if __name__ == '__main__':
+def main():
     url = "http://raspberrypi.local/?action=stream"
     # VideoCaptureのインスタンスを作成する。
-    cap = cv2.VideoCapture(url) # カメラシステムを使う場合
+    cap = cv2.VideoCapture(url)  # カメラシステムを使う場合
     if not cap.isOpened():
         print("画像のキャプチャに失敗しました")
         sys.exit()
@@ -71,10 +77,10 @@ if __name__ == '__main__':
     ret, img = cap.read()
     wname = "MouseEvent"
     cv2.namedWindow(wname)
-    npoints = 4
-    ptlist = PointList(npoints)
-    cv2.setMouseCallback(wname, get_point, [wname, img, ptlist])
-    cv2.imshow(wname, img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    ptlist = PointList()
+    ptlist.add_point()
     ptlist.trans()
+
+
+if __name__ == '__main__':
+    main()
