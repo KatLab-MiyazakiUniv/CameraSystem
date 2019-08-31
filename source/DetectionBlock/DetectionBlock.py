d8e8bbd1168f357fa1f70786184615bef26c20b3
Futa Hirakoba <korosuke613613@gmail.com>
2019-09-01 01:22:14 +0900

    create DetectionBlock

---
+++ source/DetectionBlock/DetectionBlock.py
@@ -0,0 +1,14 @@
+from BlockBingo import BlockBingo
+
+
+class DetectionBlock:
+    def __init__(self):
+        self.block_bingo = BlockBingo()
+
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    db = DetectionBlock()
+    db.start()
