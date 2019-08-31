7e97a50ec8572c612943799d4ebb430ffafb9894
Futa Hirakoba <korosuke613613@gmail.com>
2019-09-01 00:08:59 +0900

    テストの追加

--- source/DetectionBlock/test_BlockBingo.py
+++ source/DetectionBlock/test_BlockBingo.py
@@ -23,9 +23,23 @@ def test_line_direction(block_bingo):
             assert circle.lines["down"]["circle"] == block_bingo.crossCircles[circle.number + 4]
 
 
+def test_line_num(block_bingo):
+    for circle in block_bingo.crossCircles:
+        if not(0 <= circle.number <= 4):
+            assert circle.lines[circle.number - 4]["circle"] == block_bingo.crossCircles[circle.number - 4]
+        if not (circle.number % 4 == 0):
+            assert circle.lines[circle.number - 1]["circle"] == block_bingo.crossCircles[circle.number - 1]
+        if not (circle.number % 4 == 3):
+            assert circle.lines[circle.number + 1]["circle"] == block_bingo.crossCircles[circle.number + 1]
+        if not (12 <= circle.number <= 15):
+            assert circle.lines[circle.number + 4]["circle"] == block_bingo.crossCircles[circle.number + 4]
+
+
 def test_get_direction_route(block_bingo):
-    assert block_bingo.get_direction_route([0, 1, 2, 6, 10, 11, 15]) == ['right', 'right', 'down', 'down', 'right', 'down']
+    assert block_bingo.get_direction_route([0, 1, 2, 6, 10, 11, 15]) \
+           == ['right', 'right', 'down', 'down', 'right', 'down']
 
 
 def test_get_num_route(block_bingo):
-    assert block_bingo.get_num_route(['right', 'right', 'down', 'down', 'right', 'down'], 0) == [0, 1, 2, 6, 10, 11, 15]
+    assert block_bingo.get_num_route(['right', 'right', 'down', 'down', 'right', 'down'], 0) \
+           == [0, 1, 2, 6, 10, 11, 15]
