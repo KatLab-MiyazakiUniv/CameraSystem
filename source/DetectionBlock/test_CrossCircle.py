a5af578fe904af9b37aad9dd9081343395e67d35
Futa Hirakoba <korosuke613613@gmail.com>
2019-08-31 17:48:08 +0900

    create test case CrossCircle

---
+++ source/DetectionBlock/test_CrossCircle.py
@@ -0,0 +1,37 @@
+import pytest
+from DetectionBlock.BlockBingo import CrossCircle, Color, Block
+
+color = Color.RED
+
+
+@pytest.fixture
+def red_circle():
+    return CrossCircle(color)
+
+
+@pytest.fixture
+def blue_block():
+    return Block(Color.BLUE)
+
+
+def test_get_color(red_circle):
+    assert red_circle.get_color() == color
+
+
+def test_set_block(red_circle, blue_block):
+    assert red_circle.get_block() is None
+    red_circle.set_block(blue_block)
+    assert red_circle.get_block() is blue_block
+
+
+def test_remove_block(red_circle, blue_block):
+    red_circle.set_block(blue_block)
+    red_circle.remove_block()
+    assert red_circle.get_block() is None
+
+
+def test_default_line(red_circle):
+    assert red_circle.lines[0][1] is None
+    assert red_circle.lines[0][-1] is None
+    assert red_circle.lines[1][0] is None
+    assert red_circle.lines[-1][0] is None
