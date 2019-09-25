6de6d0155ec0e7b34bd9b8937fa406ac7e400c4d
Futa Hirakoba <korosuke613613@gmail.com>
2019-09-26 00:23:58 +0900

    整備

--- source/CameraSystem.py
+++ source/CameraSystem.py
@@ -9,8 +9,9 @@ class CameraSystem:
         self.camera = Camera()
         self.bt = Bluetooth()
         self.port = "/dev/cu.MindstormsEV3-SerialPor"
+        self.card_number = None
 
-    def connect_to_ev3(self):
+    def _connect_to_ev3(self):
         self.bt.connect(self.port)
         while True:
             write_data = 0
@@ -20,23 +21,26 @@ class CameraSystem:
 
             time.sleep(1)  # sec
 
-    def start(self):
-        self.connect_to_ev3()
-        print("Success! Connected ev3")
-        while True:
-            try:
-                self.bt.write(ord(input()))
-            except OverflowError:
-                print("OverflowError")
-            except ValueError:
-                print("ValueError")
-        print()
-        print("Detection Number")
+    def _detection_number(self):
         self.camera.capture()
         number_card = self.camera.get_point()
         detection_number = DetectionNumber(img=number_card, model_path="./DetectionNumber/my_model.npz")
-        number = detection_number.get_detect_number()
-        print(f"number is {number}")
+        return detection_number.get_detect_number()
+
+    def start(self):
+        print("\nConnect EV3")
+        self._connect_to_ev3()
+        print("Success! Connected ev3")
+
+        print("\nDetection Number")
+        self.card_number = self._detection_number()
+        print(f"number is {self.card_number}")
+
+        print("\nDetection Block")
+        # TODO これから
+
+        print("\nSend Command")
+        # TODO これから
 
 
 if __name__ == '__main__':
