f89bd4e111b929627502dcdaadf951390b14f45e
Futa Hirakoba <korosuke613613@gmail.com>
2019-09-26 00:30:52 +0900

    add comment

--- source/CameraSystem.py
+++ source/CameraSystem.py
@@ -12,6 +12,9 @@ class CameraSystem:
         self.card_number = None
 
     def _connect_to_ev3(self):
+        """
+        EV3とBT接続
+        """
         self.bt.connect(self.port)
         while True:
             write_data = 0
@@ -22,12 +25,33 @@ class CameraSystem:
             time.sleep(1)  # sec
 
     def _detection_number(self):
+        """
+        数字カードの切り取りと数字の識別
+        :return: 数字カードの数字
+        """
         self.camera.capture()
         number_card = self.camera.get_point()
         detection_number = DetectionNumber(img=number_card, model_path="./DetectionNumber/my_model.npz")
         return detection_number.get_detect_number()
 
+    def _send_command(self, commands):
+        """
+        EV3にコマンドを送る
+
+        :param commands: コマンドのリスト
+        """
+        for c in commands:
+            self.bt.write(ord(c))
+            time.sleep(1)  # sec
+
+        # 終了コードを送信
+        self.bt.write(ord('z'))
+
     def start(self):
+        """
+        カメラシステムクラスのメイン関数
+        :return:
+        """
         print("\nConnect EV3")
         self._connect_to_ev3()
         print("Success! Connected ev3")
@@ -38,8 +62,10 @@ class CameraSystem:
 
         print("\nDetection Block")
         # TODO これから
+        commands = ['b', 'c', 'd']
 
         print("\nSend Command")
+        self._send_command(commands)
         # TODO これから
 
 
