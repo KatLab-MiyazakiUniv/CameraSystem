37ea94ce9119f23d57c8669779419f8b320f693a
T.Miyaji <miyaji@earth.cs.miyazaki-u.ac.jp>
2019-09-26 20:38:17 +0900

    テストコードを書いた

--- source/detection_block/BlockRecognizer.py
+++ source/detection_block/BlockRecognizer.py
@@ -1,7 +1,9 @@
 from BlockBingo import BlockBingo
 from BlockBingo import Color
 import cv2
+import sys
 import numpy as np
+import pytest
 
 class BlockRecognizer:
     def __init__(self):
@@ -34,10 +36,15 @@ class BlockRecognizer:
         print(h, s, v)
         return None
         
-    def recognize_block_circle(self, img, points):
+    def recognize_block_circle(self, img, circles_coordinates):
+        # ブロックサークルの数字を削除する。画像の周辺のノイズも削除する。
         img = self.extractor.remove_circle_number(img)
-        black = None
-        color = None
+        black = None # 黒ブロックが置かれているブロックサークル番号
+        color = None # カラーブロックが置かれているブロックサークル番号
+        
+        # サークル座標のデータ構造からブロックサークルの座標だけ抽出する
+        points = self.extract_block_circles_point(circles_coordinates)
+        
         for (idx, point) in enumerate(points):
             crop = self.extractor.trim(img, point)
             if Color.BLACK == self.detect_color(self.extractor.closing(crop)):
@@ -46,6 +53,12 @@ class BlockRecognizer:
                 color = idx + 1
         
         return (black, color)   
+
+    def extract_block_circles_point(self, circles_coordinates):
+        points = []
+        for key in ['b' + str(i+1) for i in range(0, 8)]:
+            points.append(circles_coordinates[key])
+        return points
 
 class BlockExtractor():
     def trim(self, img, point, margin = 5):
@@ -119,7 +132,7 @@ class BlockExtractor():
 
     def remove_circle_number(self, img):
         """
-        ブロックサークルの数字を削除する。画像の周辺のノイズも削除する
+        ブロックサークルの数字を削除する。画像の周辺のノイズも削除する。
                 
         Parameters
         ----------
@@ -134,8 +147,34 @@ class BlockExtractor():
         # 不要な情報が黒色になっているので白色に変換する
         return cv2.addWeighted(dst, 1, cv2.bitwise_not(mask), 1, 0)
 
-if __name__ == '__main__':
-    img = cv2.imread('result.png')
+def test_result():
+    circles_coordinates = {
+        "c00": None, "c10": None, "c20": None, "c30": None,
+        "b1": (122, 153), "b2": (302, 155), "b3": (480, 150),
+        "c01": None, "c11": None, "c21": None, "c31": None,
+        "b4": (127, 338), "b5": (480, 339),
+        "c02": None, "c12": None, "c22": None, "c32": None,
+        "b6": (130, 521), "b7": (307, 521), "b8": (481, 520),
+        "c03": None, "c13": None, "c23": None, "c33": None,
+        }
     recognizer = BlockRecognizer()
-    points = [ (130, 151), (311, 152), (493, 152), (127, 345), (483, 331), (126, 537), (305, 532), (480, 534)]
-    print(recognizer.recognize_block_circle(img, points))
+    img = cv2.imread('result.png')
+    return recognizer.recognize_block_circle(img, circles_coordinates)
+
+def test_result1():
+    circles_coordinates = {
+        "c00": None, "c10": None, "c20": None, "c30": None,
+        "b1": (130, 145), "b2": (304, 148), "b3": (487, 147),
+        "c01": None, "c11": None, "c21": None, "c31": None,
+        "b4": (131, 331), "b5": (484, 333),
+        "c02": None, "c12": None, "c22": None, "c32": None,
+        "b6": (133, 517), "b7": (311, 517), "b8": (483, 515),
+        "c03": None, "c13": None, "c23": None, "c33": None,
+        } 
+    recognizer = BlockRecognizer()
+    img = cv2.imread('result1.png')
+    return recognizer.recognize_block_circle(img, circles_coordinates)
+   
+if __name__ == '__main__':
+    assert (5, 7) == test_result()
+    assert (5, 2) == test_result1()
