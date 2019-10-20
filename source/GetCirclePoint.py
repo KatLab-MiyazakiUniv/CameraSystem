b926e6d871fbd5cd317be405a05f15a5d708fd70
Tatsumi <ti18027@student.miyazaki-u.ac.jp>
2019-10-20 19:29:49 +0900

    Add: とりあえず

---
+++ source/GetCirclePoint.py
@@ -0,0 +1,19 @@
+"""
+@file: GetCirclePoint.py
+@author: Tatsumi0000
+@brief: 左上の（数字の1）の黄色い交点サークル4箇所とブロックサークル1をクリックするとすべてのサークルの座標を取得する
+"""
+
+
+class GetCirclePoint:
+
+    def __init__(self):
+        """コンストラクタ
+        """
+        pass
+
+    def captureCamera(self):
+        """
+        動画をキャプチャして画像として切り出して保存する
+        :return:
+        """
