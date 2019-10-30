6ba2a93e5e281c6be3478cf2125e3d7ebcbf1b11
T.Miyaji <thomath0901@gmail.com>
2019-10-30 11:22:00 +0900

    回頭コマンドに変換する処理を書いた

--- source/block_bingo/commands.py
+++ source/block_bingo/commands.py
@@ -6,6 +6,28 @@
 from block_bingo_coordinate import BlockCirclesCoordinate
 from block_bingo_coordinate import CrossCirclesCoordinate
 
+class Instructions():
+    ENTER_BINGO_AREA_L4 = 'a'
+    ENTER_BINGO_AREA_L6 = 'b'
+    STRAIGHT = 'c'
+    SPIN_RIGHT = 'd'
+    SPIN_LEFT = 'e'
+    SPIN180 = 'f'
+    PUT = 'g'
+
+
+    def translate(self, instruction):
+        ja = {Instructions.ENTER_BINGO_AREA_L4: '4番サークルに進入',
+              Instructions.ENTER_BINGO_AREA_L6: '6番サークルに進入',
+              Instructions.STRAIGHT: 'ブロックサークル間を直進',
+              Instructions.SPIN_RIGHT: '右に90°回頭する',
+              Instructions.SPIN_LEFT: '左に90°回頭する',
+              Instructions.SPIN180: '180°回頭する',
+              Instructions.PUT: 'ブロックを黒線の中点から設置',
+        }
+        return ja[instruction]
+
+
 class Commands():
     def __init__(self, block_circles, cross_circles):
         """
@@ -20,3 +42,84 @@ class Commands():
         """
         self.block_circles = block_circles
         self.cross_circles = cross_circles
+
+        # コマンドのリスト
+        self.commands = []
+    
+    
+    def convert(self, src, dst, direction, path):
+        """
+        運搬経路をコマンド変換する。
+
+        Parameters
+        ----------
+        src : tuple
+            始点の座標
+        dst : tuple
+            終点の座標
+        direction : int
+            方角: 上向きを0とし、時計回りに45度=1、90度=2、135度=3・・・315度=7とする
+            0
+          6   2
+            4
+        path : list
+            運搬経路（移動する座標のリスト）
+        """
+        # 始点にブロックが置いてあるか調べる。
+        has_block = src in self.cross_circles.open
+        # srcの前に運搬経路が存在しないことを確認する。
+        if src == path[0]:
+            self.spin(src, dst, direction, has_block)
+    
+
+    def get(self):
+        return self.commands
+
+
+    def get_next_direction(self, src, dst):
+        """
+        始点から終点までの移動したときの走行体の向きを取得する。
+        方角: 上向きを0とし、時計回りに45度=1、90度=2、135度=3・・・315度=7とする
+            0
+          6   2
+            4
+        Parameters
+        ----------
+        src : tuple
+            始点の座標
+        dst : tuple
+            終点の座標
+        """
+        subtraction = (dst[0]-src[0], dst[1]-src[1])
+
+        if subtraction == (-1, 0) or subtraction == (-0.5, 0):
+            return 0    # 北向き
+        if subtraction == (0, 1) or subtraction == (0, 0.5):
+            return 2    # 東向き
+        if subtraction == (1, 0) or subtraction == (0.5, 0):
+            return 4    # 南向き
+        if subtraction == (0, -1) or subtraction == (0, -0.5):
+            return 6    # 西向き
+        raise ValueError('could not calculate direction')
+
+
+    def spin(self, src, dst, direction, has_block):
+        """
+        回頭コマンドへ変換する。
+        """
+        next_direction = self.get_next_direction(src, dst)
+        
+        # 次の方向と現在の方向を引き算する。
+        sub = next_direction - direction
+
+        if sub == 2 or sub == 6:
+            self.commands.append(Instructions.SPIN_RIGHT)
+            return next_direction
+        if sub == 4 or sub == -4:
+            self.commands.append(Instructions.SPIN180)
+            return next_direction
+        if sub == -2 or sub == -6:
+            self.commands.append(Instructions.SPIN_LEFT)
+            return next_direction
+        
+        raise ArithmeticError('cannot convert path to SPIN command!')
---
+++ source/block_bingo/test_commands.py
@@ -0,0 +1,44 @@
+"""
+    @file   test_commands.py
+    @author T.Miyaji
+    @brief  commandsのテストコード
+"""
+from commands import Instructions
+from commands import Commands
+from block_bingo_coordinate import BlockCirclesCoordinate
+from block_bingo_coordinate import CrossCirclesCoordinate
+import pytest
+
+def test_translate():
+    order = [Instructions.ENTER_BINGO_AREA_L6,
+             Instructions.STRAIGHT,
+             Instructions.STRAIGHT,
+             Instructions.SPIN_LEFT,
+             Instructions.PUT]
+    instructions = Instructions()
+    order = [instructions.translate(e) for e in order]
+
+    assert order[0] == '6番サークルに進入'
+    assert order[1] == 'ブロックサークル間を直進'
+    assert order[2] == 'ブロックサークル間を直進'
+    assert order[3] == '左に90°回頭する'
+    assert order[4] == 'ブロックを黒線の中点から設置'
+
+
+def create_commands(is_left=True, bonus=5, color=3):
+    block_circles = BlockCirclesCoordinate(is_left, bonus, color)
+    cross_circles = CrossCirclesCoordinate()
+
+    return Commands(block_circles, cross_circles)
+
+def test_spin():
+    commands = create_commands()
+    
+    src = (3,1)
+    dst = (2.5,1)
+    direction = 4   # 南に向いていると仮定
+    has_block = False
+
+    assert 0 == commands.spin(src, dst, direction, has_block)
+    assert 1 == len(commands.get())
+    assert Instructions.SPIN180 == commands.get()[0]
