9575bac6a6185bffd7ed7a6de26a0d8dd085579a
Tatsumi <ti18027@student.miyazaki-u.ac.jp>
2019-11-01 13:18:07 +0900

    Fix: メソッド名とかを命名規則通りに修正

--- source/decision_points/MoveGetCirclePoint.py
+++ source/decision_points/MoveGetCirclePoint.py
@@ -7,7 +7,7 @@
 import cv2.cv2 as cv2
 import tkinter as tk
 from PIL import Image, ImageTk
-from source.decision_points import get_circle_point as gcp
+from source.decision_points import GetCirclePoint as gcp
 
 
 class Points:
@@ -23,15 +23,12 @@ class MoveGetCirclePoint(tk.Frame):
         """コンストラクタ
         """
         super().__init__(master)
-        self.master = master
         self.RADIUS = 5  # 描画する円の半径
-        self.master.bind("<KeyPress>", self.keyEvent)  # キーボードを入力を受け付ける
-        # master.focus_set()
+        self.master.bind("<KeyPress>", self.key_event)  # キーボードを入力を受け付ける
         self.img = Image.open(img)
         self.image_tk = ImageTk.PhotoImage(self.img)
         width, height = self.img.size  # 画像のサイズ
         self.points = Points()
-        self.title = title  # windowのタイトル
         self.get_circle_point = gcp.GetCirclePoint()
         self.master.title(title)
         self.canvas = tk.Canvas(self.master, width=width, height=height)  # Canvas作成
@@ -39,7 +36,7 @@ class MoveGetCirclePoint(tk.Frame):
         self.canvas.create_image(0, 0, image=self.image_tk, anchor=tk.NW)  # ImageTk画像配置
         self.canvas.pack()
 
-    def setGetCirclePoint(self, get_circle_point):
+    def set_get_circle_point(self, get_circle_point):
         """
         get_circle_pointのセッター
         :param get_circle_point:
@@ -48,9 +45,9 @@ class MoveGetCirclePoint(tk.Frame):
         """
         self.get_circle_point = get_circle_point
 
-    def drawGetCirclePoint(self):
+    def draw_get_circle_point(self):
         """
-        メモ：：ここのfor文を辞書型の方で回して，タグの名前をその辞書型のkeyに変えることで後でソートが楽になるはず
+        座標を描画する．
         :return:
         """
         for key, value in self.get_circle_point.named_points.items():
@@ -65,27 +62,27 @@ class MoveGetCirclePoint(tk.Frame):
                 self.canvas.create_oval(self.get_circle_point.named_points[key][0] - self.RADIUS,  # 左上角のx座標
                                         self.get_circle_point.named_points[key][1] - self.RADIUS,  # 左上角のy座標
                                         self.get_circle_point.named_points[key][0] + self.RADIUS,  # 右下角のx座標
-                                        self.get_circle_point.named_points[key][1] + self.RADIUS, fill='green',
+                                        self.get_circle_point.named_points[key][1] + self.RADIUS, fill='yellow',
                                         # 右下角のx座標, 塗りつぶす色
                                         tags='{0}'.format(key))  # tags=オブジェクト固有の番号
-            self.canvas.tag_bind('{0}'.format(key), "<ButtonPress-1>", self.mousePressed)
-            self.canvas.tag_bind('{0}'.format(key), "<B1-Motion>", self.mouseDragged)
+            self.canvas.tag_bind('{0}'.format(key), "<ButtonPress-1>", self.mouse_pressed)  # 右クリックの処理
+            self.canvas.tag_bind('{0}'.format(key), "<B1-Motion>", self.mouse_dragged)  # マウスの動き
 
-    def mousePressed(self, event):
+    def mouse_pressed(self, event):
         """
         マウスが押されると呼ばれる
         :param event:
         :return:
         """
-        self.points.item_id = self.canvas.find_closest(event.x, event.y)
-        print(self.canvas.find_closest(event.x, event.y))
-        tag = self.canvas.gettags(self.points.item_id[0])[0]
-        item = self.canvas.type(tag)
-        print('押されたのは：{}'.format(item))
+        self.points.item_id = self.canvas.find_closest(event.x, event.y)  # クリックされた座標を渡して何がクリックされたかidを探す
+        # print(self.canvas.find_closest(event.x, event.y))
+        # tag = self.canvas.gettags(self.points.item_id[0])[0]  # idを渡してtagを探す
+        # item = self.canvas.type(tag)
+        # print('押されたのは：{}'.format(item))
         self.points.ix = event.x
         self.points.iy = event.y
 
-    def mouseDragged(self, event):
+    def mouse_dragged(self, event):
         """
         マウスがドラッグした時に呼ばれる
         :param event:
@@ -93,8 +90,8 @@ class MoveGetCirclePoint(tk.Frame):
         """
         try:
             self.points.item_id = self.canvas.find_closest(event.x, event.y)
-            tag = self.canvas.gettags(self.points.item_id[0])[0]
-            item = self.canvas.type(tag)
+            tag = self.canvas.gettags(self.points.item_id[0])[0]  # idを渡してtagを探す
+            item = self.canvas.type(tag)  # タグを元に図形の形（？）を取得
             delta_x = event.x - self.points.ix
             delta_y = event.y - self.points.iy
             if item == 'oval':  # 円のとき
@@ -105,15 +102,15 @@ class MoveGetCirclePoint(tk.Frame):
         except IndexError:
             print('マウスの動きが早すぎます．')
 
-    def keyEvent(self, event):
+    def key_event(self, event):
         if event.char == 'q':  # キーボードのqが押されたら
-            self.fixPoint()
+            self.fix_point()
             self.master.quit()  # tkinterを終了
         if event.char == 'd':  # デバッグ用のd
-            self.fixPoint()
+            self.fix_point()
         print('押されたキーボード：{}'.format(event.char))
 
-    def fixPoint(self):
+    def fix_point(self):
         """
         画面上のすべての座標を取得しnamed_pointsに代入する．
         本当は，マウスイベントが起こった時に対応する辞書型に代入したほうが圧倒的に効率的
@@ -131,13 +128,13 @@ class MoveGetCirclePoint(tk.Frame):
                     self.get_circle_point.named_points[tag[0]] = [x, y]
         print('修正後： {0}'.format(self.get_circle_point.named_points))
 
-    def runGetCirclePoint(self):
+    def run_get_circle_point(self):
         img = './../img/clip_field.png'
         window_name = 'WindowDAYO'
         img = cv2.imread(img)
-        self.setGetCirclePoint(gcp.GetCirclePoint(window_name=window_name))
+        self.set_get_circle_point(gcp.GetCirclePoint(window_name=window_name))
         cv2.namedWindow(window_name)
-        cv2.setMouseCallback(window_name, self.get_circle_point.dragAndDropSquare,
+        cv2.setMouseCallback(window_name, self.get_circle_point.drag_and_drop_square,
                              [window_name, img, self.get_circle_point])
         cv2.imshow(window_name, img)
         cv2.moveWindow(window_name, 100, 100)  # 左上にウィンドウを出す
@@ -146,10 +143,10 @@ class MoveGetCirclePoint(tk.Frame):
 
     def run(self):
         print('プログラムが起動しました．ウィンドウが出ない場合は，後ろにウィンドウがでてるかもしれません．')
-        self.runGetCirclePoint()
-        self.drawGetCirclePoint()
+        self.run_get_circle_point()
+        self.draw_get_circle_point()
         self.mainloop()
-        # self.fixPoint()
+        # self.fix_point()
         # self.debug()
 
 
